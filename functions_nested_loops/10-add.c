f5d11779465b66f3f434b3d8aa50bb44801a8ed1
Ntombikayise Kidibone Makola <ntombikayise.makola@gmail.com>
2024-03-07 12:46:09 +0000

    10. a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: c; sum of a and b
+*/
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
