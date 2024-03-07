d8bbd6efebbffd5ab241d84bd7fd158492c745c0
Ntombikayise Kidibone Makola <ntombikayise.makola@gmail.com>
2024-03-07 08:02:22 +0000

    1. I sometimes suffer from insomnia. And when I cant fall asleep, I play what I call the alphabet game

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+
+	_putchar('\n');
+}
