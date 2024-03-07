89bb7e45952f9615d507e282574a3fa0f65d3379
Ntombikayise Kidibone Makola <ntombikayise.makola@gmail.com>
2024-03-07 09:02:13 +0000

    2. 10 x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print lowercase alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int k;
+	char alphabet;
+	
+	for (k = 0; k <= 10; k++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+	_putchar('\n');
+	}
+}
