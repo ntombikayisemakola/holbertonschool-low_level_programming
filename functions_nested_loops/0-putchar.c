a7a6070d360755e1a16c968b428f5df342a24fe8
Ntombikayise Kidibone Makola <ntombikayise.makola@gmail.com>
2024-03-07 07:10:39 +0000

    0. _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * main - Entry point
+ * main - print _putchar (Entry point)
  *
  * Return: Always 0 (success)
 */
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
+
 }
