7731e5ce77574181e74a386588a6fcaad71ea809
Ntombikayise Kidibone Makola <ntombikayise.makola@gmail.com>
2024-03-12 09:50:53 +0000

    2. This report, by its very length, defends itself against the risk of being read

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strlen - finds the length of a string.
+* @s: string pointer to the string whose length is to be found.
+* Return: returns the length of the string.
+*/
+
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+}
